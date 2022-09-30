0862b62154e910e056b4b1a4bd3e6157a5522a70
Nick Winkler <nick@Nicks-MacBook-Air.local>
2022-09-30 15:56:43 -0700

    hw1 initial commit

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -11,7 +11,7 @@ Use console.log() to write the proper output to the command line.
 
 const fizzbuzz = () => {
   // Add your code here
-  for (let i = 0; i < 100; i++) {
+  for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
       console.log("fizzbuzz");
     } else if (i % 3 == 0) {
