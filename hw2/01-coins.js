24d5234368607097d53c346479ce64f7e46932ef
Nick Winkler <nick@Nicks-MacBook-Air.local>
2022-10-13 15:43:33 -0700

    initial commit for hw2/reverse

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 /** Exercise 01 - Coins **/
-//TODO Need to figure out commas
+//TODO What about values with just dollar ammounts or values with zeros IE: 0.20
 import "./styles.css";
 
 const calculateChange = (input) => {
--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,3 +1,49 @@
 /** Exercise 02 - Reverse **/
-
+let input = document.getElementById("input");
+let submit = document.getElementById("reverse");
+let form = document.getElementById("form");
 // Add your code here
+
+let reverse = () => {
+    let number = input.value;
+    let numString = number.toString();
+
+    //looks to see if we already have an output message element
+    let outputElement = document.getElementById("message");
+
+    //create a new element for displaying to user
+    let output = document.createElement("p");
+    output.id = "message";
+    
+    //if we have an output message in dom, remove it for this event
+    if(outputElement){
+        form.removeChild(outputElement)
+    }
+    
+    if(numString.length === 8) {
+        
+        let arr = numString.split('');
+        arr = arr.reverse();
+        numString = arr.join('');
+
+        output.innerHTML = `${number} --> ${numString}`;
+        
+    }
+
+    else {
+     output.innerHTML = 'Error: Please input an 8-digit number';
+     output.style.color = "red";
+     
+    }
+
+    form.appendChild(output);
+
+}
+
+submit.addEventListener("click", reverse);
+
+
+
+
+
+
